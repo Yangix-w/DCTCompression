ff1a0ff88d24ae7e234e57a2f2818492f754efda
Yangix <l.shi1@campus.unimib.it>
2026-05-29 15:40:35 +0200

    Changed the matrix

--- Part1/src/main/java/org/example/Main.java
+++ Part1/src/main/java/org/example/Main.java
@@ -4,12 +4,17 @@ import org.ejml.simple.SimpleMatrix;
 import org.example.custom_dct.DCT;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         DCT dct = new DCT();
         SimpleMatrix f = new SimpleMatrix(new double[][]{
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {231, 32, 233, 161, 24, 71, 140, 245},
+                {247, 40, 248, 245, 124, 204, 36, 107},
+                {234, 202, 245, 167, 9, 217, 239, 173},
+                {193, 190, 100, 167, 43, 180, 8, 70},
+                {11, 24, 210, 177, 81, 243, 8, 112},
+                {97, 195, 203, 47, 125, 114, 165, 181},
+                {193, 70, 174, 167, 41, 30, 127, 245},
+                {87, 149, 57, 192, 65, 129, 178, 228}
         });
         SimpleMatrix c = dct.dct2(f);
         System.out.println("DCT of f:");
