2464c5b475f734fa29e8e0db45464d6e9b46ae67
Yangix <l.shi1@campus.unimib.it>
2026-06-01 15:35:06 +0200

    Refactor main class

--- Part1/src/main/java/org/example/Main.java
+++ Part1/src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
-import org.ejml.simple.SimpleMatrix;
-import org.example.custom_dct.DCT;
-import org.jtransforms.dct.DoubleDCT_2D;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import org.ejml.simple.SimpleMatrix;
+import org.example.custom_dct.DCT;
+import org.jtransforms.dct.DoubleDCT_2D;
+
 public class Main {
     private static final int CONVERT_IN_MILLI = 1_000_000;
 
     public static void main(String[] args) {
         //test();
+        benchmarkDCT();
+    }
+
+    public static void benchmarkDCT(){
         int n = 1000; //Limite massima delle matrici
         double[][] matrix;
         SimpleMatrix sm;
@@ -38,46 +43,45 @@ public class Main {
             fftDct.forward(matrix, true);
             endTime = System.nanoTime();
             fftDctTime.add((double)(endTime - startTime)/CONVERT_IN_MILLI); // Converti in millisecondi
-         }
+        }
+        // Salva i dati in CSV nella cartella results
+        saveToCsv("./Part1/results/dct_times.csv", customDctTime, fftDctTime);
+    }
 
-         // Salva i dati in CSV nella cartella results
-         saveToCsv("results/dct_times.csv", customDctTime, fftDctTime);
-     }
-
-    public static double[][] generateMatrix(int n){
-         double[][] matrix = new double[n][n];
-         for(int i = 0; i < n; i++){
-             for(int j = 0; j < n; j++){
-                 matrix[i][j] = Math.random()*100;
+        public static double[][] generateMatrix(int n){
+             double[][] matrix = new double[n][n];
+             for(int i = 0; i < n; i++){
+                 for(int j = 0; j < n; j++){
+                     matrix[i][j] = Math.random()*100;
+                 }
              }
-         }
-         return matrix;
-     }
+             return matrix;
+        }
+        
+        public static void saveToCsv(String filename, ArrayList<Double> customDctTime, ArrayList<Double> fftDctTime) {
+            try {
+                // Crea la cartella "results" se non esiste
+                String dirPath = "results";
+                Files.createDirectories(Paths.get(dirPath));
 
-      public static void saveToCsv(String filename, ArrayList<Double> customDctTime, ArrayList<Double> fftDctTime) {
-          try {
-              // Crea la cartella "results" se non esiste
-              String dirPath = "results";
-              Files.createDirectories(Paths.get(dirPath));
+            // Scrivi il file CSV
+            try (FileWriter writer = new FileWriter(filename)) {
+                // Scrivi intestazioni
+                writer.append("Matrix Dimension,Custom DCT Time (ms),JTransforms DCT Time (ms)\n");
 
-              // Scrivi il file CSV
-              try (FileWriter writer = new FileWriter(filename)) {
-                  // Scrivi intestazioni
-                  writer.append("Matrix Dimension,Custom DCT Time (ms),JTransforms DCT Time (ms)\n");
+                // Scrivi i dati
+                for (int i = 0; i < customDctTime.size(); i++) {
+                    int dimension = 10 + (i * 10);
+                    writer.append(String.valueOf(dimension)).append(",").append(String.valueOf(customDctTime.get(i))).append(",").append(String.valueOf(fftDctTime.get(i))).append("\n");
+                }
 
-                  // Scrivi i dati
-                  for (int i = 0; i < customDctTime.size(); i++) {
-                      int dimension = 10 + (i * 10);
-                      writer.append(String.valueOf(dimension)).append(",").append(String.valueOf(customDctTime.get(i))).append(",").append(String.valueOf(fftDctTime.get(i))).append("\n");
-                  }
-
-                  System.out.println("File CSV salvato: " + filename);
-              }
-          } catch (IOException e) {
-              System.err.println("Errore durante la scrittura del file CSV: " + e.getMessage());
-              e.printStackTrace();
-          }
-      }
+                System.out.println("File CSV salvato: " + filename);
+            }
+        } catch (IOException e) {
+            System.err.println("Errore durante la scrittura del file CSV: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
     public static void test(){
         SimpleMatrix m = new SimpleMatrix(new double[][]{
                 {231, 32, 233, 161, 24, 71, 140, 245},
@@ -106,7 +110,6 @@ public class Main {
                 System.out.print(aDouble + " ");
             }
         }
-
-
     }
+
 }
